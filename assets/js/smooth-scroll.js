42ce5162fa80763fedca2914cdcf022311e5e032
Danial-hakim <58420356+Danial-hakim@users.noreply.github.com>
2023-07-17 16:20:56 +0100

    Upload portfolio

---
+++ assets/js/smooth-scroll.js
@@ -0,0 +1,13 @@
+// Smooth scrolling behavior
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(event) {
+      var target = $(this.getAttribute('href'));
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+          scrollTop: target.offset().top
+        }, 800); // Adjust the scroll speed if desired
+      }
+    });
+  });
+  
